451f3e4784c2255114b657ffcb3ea94266ba4be7
Roman Sakh <gribochechek@gmail.com>
2019-12-15 13:44:18 +0200

    First commit

---
+++ TestAutomationTasks/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestAutomationTasks
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            // Creates a new Chrome instance and opens the browser
+            driver.Navigate().GoToUrl("https://www.bbc.com");
+            // Navigates to a page by address
+
+
+        }
+    }
+}
---
+++ TestAutomationTasks/UnitTest2.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+
+namespace TestAutomationTasks
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            // Creates a new Chrome instance and opens the browser
+            driver.Navigate().GoToUrl("https://www.bbc.com");
+                        
+            IWebElement element = driver.FindElement(By.XPath("/html/body/header/div/div[1]/nav/div/ul/li[2]/a"));
+            element.Click();
+
+            
+            IWebElement el1 = driver.FindElement(By.XPath("/html/body/div[7]/div/div[4]/div[2]/div/div/div/div/div[1]/div/div/div[1]/div/a/h3"));
+            string el1Text = el1.Text;
+            Assert.AreEqual("'Appalling' attack on HK envoy in UK angers China", el1Text);
+
+
+
+            
+               
+
+
+
+
+            // Navigates to a page by address
+
+
+        }
+    }
+}
